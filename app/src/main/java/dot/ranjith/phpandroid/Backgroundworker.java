6aec3d55cd2870620a426b759b59535429c9383c
Ranjith54321 <sranjith54321@gmail.com>
2019-08-14 09:55:40 +0530

    Initial commit

---
+++ app/src/main/java/dot/ranjith/phpandroid/Backgroundworker.java
@@ -0,0 +1,147 @@
+package dot.ranjith.phpandroid;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class Backgroundworker extends AsyncTask<String,Void,String> {     // here parameters must watch
+    Context context;
+    AlertDialog alertDialog;
+    Backgroundworker (Context ctx) {
+        context = ctx;
+    }
+    @Override
+    protected String doInBackground(String... params) {
+        String type = params[0];
+        String login_url = "http://192.168.43.241:8080/login.php"; // 10.0.2.2 is to run the emulator in the laptop 8080 is the laptops portnumber
+        if(type.equals("login")) {                                 // you can use the laptops wireless kan and wifi ip address but use port numbr also then only it's run
+            try {
+                String user_name = params[1];
+                String password = params[2];
+                URL url = new URL(login_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+"&"
+                        +URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result="";
+                String line="";
+                while((line = bufferedReader.readLine())!= null) {
+                    result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle("Login Status");
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        alertDialog.setMessage(result);
+        alertDialog.show();
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+}/*
+    Context context;     // context is used for create object in the main_activity
+    Backgroundworker(Context ctx){
+        context = ctx;
+    }
+    AlertDialog alertDialog;
+    @Override
+    protected String doInBackground(String... params) { // you need to change the type here also(insie parameter)  2.returntype also changed
+        String type = params[0];
+        String login_url = "http://192.168.43.241/login.php";  // here only you give the url of the server
+        if(type.equals("login")){
+            try {
+                String user_name = params[1];
+                String password = params[2];
+                URL url = new URL(login_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection(); // type caste get the url through string and then type caste it into Http ** this creates the exception **2**
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+"&"
+                        +URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result="";
+                String line="";
+                while((line = bufferedReader.readLine())!=null){
+                    result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace(); // **2 this exception is fromed by httpconnection *****
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle("Login Status");
+    }
+
+    @Override
+    protected void onPostExecute(String result) {  // here changed to Sting*******  here shows the result of the php result form login.php
+        alertDialog.setMessage(result);
+        alertDialog.show();
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+}
+
+*/
---
+++ app/src/main/java/dot/ranjith/phpandroid/MainActivity.java
@@ -0,0 +1,42 @@
+package dot.ranjith.phpandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button bt;
+    EditText t1,t2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        t1=(EditText)findViewById(R.id.t1);
+        t2=(EditText)findViewById(R.id.t2);
+        bt=(Button)findViewById(R.id.bt);
+
+/*        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = t1.getText().toString();
+                String password = t2.getText().toString();
+                String type = "login";             //********* here backeorker can't able to carete object and pass the valuse
+                Backgroundworker backgroundworker = new Backgroundworker(this);  //**** so use the onclick in xml and then use it
+                backgroundworker.execute(type,username,password);
+            }
+        });   */
+    }
+
+    public void Onlogin(View view){
+             String username = t1.getText().toString();
+             String password = t2.getText().toString();
+             String type = "login";
+
+             Backgroundworker backgroundworker = new Backgroundworker(this);
+             backgroundworker.execute(type,username,password);
+    }
+}
